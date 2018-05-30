ce595d9c046a354c52d16eb69f0361641da64a8b
pavelspinu <39630048+pavelspinu@users.noreply.github.com>
2018-05-30 16:44:22 +0300

    Add files via upload

---
+++ JavaHomeWork4.java
@@ -0,0 +1,25 @@
+/*
+ * HomeWork4 for Tekwill Java courses
+ * Spinu Pavel
+ */
+package javahomework4;
+
+/**
+ *
+ * @author Pavel
+ */
+public class JavaHomeWork4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double price, tax, Total;
+        price = 25;
+        tax = 4;
+        Total = price*tax;
+        System.out.println (("Suma totala este : ")+(Total)); 
+                
+    }
+    
+}
